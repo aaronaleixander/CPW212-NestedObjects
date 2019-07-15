5f1d139efccfa5141dee9763f903eaa9f0958698
Aaron Utterback <aar0nu@outlook.com>
2019-07-15 13:44:01 -0700

    Created the Student Class

---
+++ NestedObjectsSample/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjectsSample
+{
+    class Student
+    {
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public string  HomeAddress { get; set; }
+
+        /// <summary>
+        /// The students home phone number.
+        /// </summary>
+        public string Phone { get; set; }
+
+    }
+}
